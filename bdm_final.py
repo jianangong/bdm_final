3cc6e7ec9ba0afe9f00d4df46a63ec072198464b
jianangong <60515427+jianangong@users.noreply.github.com>
2020-05-17 09:44:41 -0400

    Update bdm_final.py

--- bdm_final.py
+++ bdm_final.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     vio_stre2= stre.join(viola, filtering2, how='left').groupBy([stre.physicalID, viola.year]).count()
     
     totalfinal=vio_stre.rdd.union(vio_stre2.rdd)
-    totalfinal.rdd.map(lambda x: ((x[0], x[1]), x[2])) \
+    totalfinal.map(lambda x: ((x[0], x[1]), x[2])) \
             .mapPartitions(breaktoyear) \
             .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4])) \
             .sortByKey() \
