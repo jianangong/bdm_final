b51cba3c7eea6c3832561c5550b699448ad81dda
jianangong <60515427+jianangong@users.noreply.github.com>
2020-05-17 16:49:30 -0400

    Update extra2.py

--- extra2.py
+++ extra2.py
@@ -12,57 +12,39 @@ from pyspark import SparkContext
 
     
     
-def getdict():
-    import csv
-    import pyproj
-    import shapely.geometry as geom
-    proj = pyproj.Proj(init='epsg:2263',preserve_units=True)
-   
+wordcount={}
+keyword=[]
+with open('drug_illegal.txt') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        keyword.append(row[0])
+with open('drug_sched2.txt') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        keyword.append(row[0])
+with open(sys.argv[1]) as file:
+    reader = csv.reader(file,delimiter='|')      
+    for row in reader:
+        if len(row) < 7:
+            continue
+        if len(row[6])<3:
+            continue
+        flag=0
+        for i in keyword:
+            if i in row[6]:
+                flag=1
+                break
+        if flag==1:
+            row[6]=row[6].replace(',','')
+            for i in row[6].split(' '):
+                wordcount[i]=wordcount.get(i,0)+1
 
-    wordcount={}
-    counts={}
-    keyword=[]
-    with open('drug_illegal.txt') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            keyword.append(row[0])
-    with open('drug_sched2.txt') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            keyword.append(row[0])
-    with open(sys.argv[1]) as file:
-        reader = csv.reader(file,delimiter='|')      
-        for row in reader:
-            if len(row) < 7:
-                continue
-            if len(row[6])<3:
-                continue
-            flag=0
-            for i in keyword:
-                if i in row[6]:
-                    flag=1
-                    break
-            if flag==1:
-                row[6]=row[6].replace(',','')
-                for i in row[6].split(' '):
-                    wordcount[i]=wordcount.get(i,0)+1
-
-    return wordcount
     
 def processwords(pid,records):
     import csv
-    worddict=getdict()
-    keyword=[]
-    counts={}
-    with open('drug_illegal.txt') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            keyword.append(row[0])
-    with open('drug_sched2.txt') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            keyword.append(row[0])
+    
    
+    counts={}
     reader = csv.reader(records,delimiter='|')    
     for row in reader:
         if len(row) < 7:
@@ -77,24 +59,21 @@ def processwords(pid,records):
                 break
         if(flag==1):
             row[6]=row[6].replace(',','')
-            print(row[6])
             for i in row[6].split(' '):
-                fre.append((i,worddict[i]))
+                fre.append((i,wordcount[i]))
             fre1=set(fre)
             a=[i[0] for i in sorted(fre1, key=lambda x : x[1])[0:3]]
-            print(a)
-
+            
             for i in a:
                 counts[i]=counts.get(i,0)+1
         else:
             continue
     return counts.items()
     
-    
 if __name__ == "__main__":
     output=sys.argv[2]
     tweetdata=sys.argv[1]
     sc = SparkContext()
-    tweet = sc.textFile(tweetdata).cache()       
+    tweet = sc.textFile('/Users/jianangong/Downloads/tweet.csv').cache()       
     freq = tweet.mapPartitionsWithIndex(processwords).top(100, key=lambda x: x[1])
     sc.parallelize(freq).saveAsTextFile(output)
