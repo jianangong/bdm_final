8eaea412ce493f27d318601e4d4bdf2110492358
jianangong <60515427+jianangong@users.noreply.github.com>
2020-05-17 17:03:07 -0400

    Update extra2.py

--- extra2.py
+++ extra2.py
@@ -9,46 +9,9 @@ import pandas as pd
 import geopandas as gpd
 from pyspark import SparkContext
 
+address1=''
 
-    
-    
-
-
-    
-def processwords(pid,records):
-    import csv
-    
-   
-    counts={}
-    reader = csv.reader(records,delimiter='|')    
-    for row in reader:
-        if len(row) < 7:
-            continue
-        if len(row[6].split(' '))<3:
-            continue
-        flag = 0
-        fre=[]
-        for i in keyword:
-            if i in row[6]:
-                flag=1
-                break
-        if(flag==1):
-            row[6]=row[6].replace(',','')
-            for i in row[6].split(' '):
-                fre.append((i,wordcount[i]))
-            fre1=set(fre)
-            a=[i[0] for i in sorted(fre1, key=lambda x : x[1])[0:3]]
-            
-            for i in a:
-                counts[i]=counts.get(i,0)+1
-        else:
-            continue
-    return counts.items()
-    
-if __name__ == "__main__":
-    output=sys.argv[2]
-    tweetdata=sys.argv[1]
-    
+def getdict():
     wordcount={}
     keyword=[]
     with open('drug_illegal.txt') as file:
@@ -59,7 +22,7 @@ if __name__ == "__main__":
         reader = csv.reader(file)
         for row in reader:
             keyword.append(row[0])
-    with open(sys.argv[1]) as file:
+    with open(address1) as file:
         reader = csv.reader(file,delimiter='|')      
         for row in reader:
             if len(row) < 7:
@@ -75,10 +38,53 @@ if __name__ == "__main__":
                 row[6]=row[6].replace(',','')
                 for i in row[6].split(' '):
                     wordcount[i]=wordcount.get(i,0)+1
+    return wordcount
+    
+
+
+    
+def processwords(pid,records):
+    import csv
+    
+    
+    worddict=getdict()
+    counts={}
+    reader = csv.reader(records,delimiter='|')   
+    
+    
+    for row in reader:
+        if len(row) < 7:
+            continue
+        if len(row[6].split(' '))<3:
+            continue
+        flag = 0
+        fre=[]
+        for i in keyword:
+            if i in row[6]:
+                flag=1
+                break
+        if(flag==1):
+            row[6]=row[6].replace(',','')
+            for i in row[6].split(' '):
+                fre.append((i,worddict[i]))
+            fre1=set(fre)
+            a=[i[0] for i in sorted(fre1, key=lambda x : x[1])[0:3]]
+            
+            for i in a:
+                counts[i]=counts.get(i,0)+1
+        else:
+            continue
+    return counts.items()
+    
+if __name__ == "__main__":
+    output=sys.argv[2]
+    tweetdata=sys.argv[1]
+    address1= sys.argv[1]
+    
     
     
     
     sc = SparkContext()
-    tweet = sc.textFile('/Users/jianangong/Downloads/tweet.csv').cache()       
+    tweet = sc.textFile(tweetdata).cache()       
     freq = tweet.mapPartitionsWithIndex(processwords).top(100, key=lambda x: x[1])
     sc.parallelize(freq).saveAsTextFile(output)
